5efde683b8220ac736b2ee63e3c89151f1a518b9
jcurbelo <curbelorobin@gmail.com>
2015-10-01 21:44:59 -0400

    Add 'listgame' py solution

---
+++ kattis/listgame/listgame.py
@@ -0,0 +1,16 @@
+def prime_factors_count(n):
+	i = 2
+	count = 0
+	while i * i <= n:
+		while n % i == 0:
+			count += 1
+			n /= i
+		i += 1
+	return count + 1 if n > 1 else count
+
+def main():
+	n = input()
+	print prime_factors_count(n)
+
+if __name__=='__main__':
+	main()
