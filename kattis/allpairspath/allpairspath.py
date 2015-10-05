60b3ac6cd771b03e11bce5827d5e003a282304f5
jcurbelo <curbelorobin@gmail.com>
2015-10-05 16:25:12 -0400

    Assumed multigraph for 'allpairspath', solution order: |V|^3

--- kattis/allpairspath/allpairspath.py
+++ kattis/allpairspath/allpairspath.py
@@ -1,43 +1,92 @@
-def floyd_warshall(g, t, c):
+def floyd_warshall(g, p, c):
 	n = len(g)
-	for k in xrange(n):
-		for i in xrange(n):
-			for j in xrange(n):
-				g[i][j] = min(g[i][j], g[i][k] + g[k][j])
-				t[i][j] = t[i][j] or (t[i][k] and t[k][j])
-
-	# Find negative cycles
-	for k in xrange(n):
-		for i in xrange(n):
-			for j in xrange(n):
+	lst = xrange(n)
+	for k in lst:
+		for i in lst:
+			for j in lst:
 				if g[i][k] + g[k][j] < g[i][j]:
+					g[i][j] = g[i][k] + g[k][j]
+					p[i][j] = p[k][j]
+
+	
+	# Find negative cycles
+	# for i in lst:
+	# 	for j in lst:
+	# 		v = p[i][j]
+	# 		flag = False
+	# 		while v:
+	# 			if g[v][v] < 0:
+	# 				c[i][j] = flag = True
+	# 				break
+	# 			# little case for non negative self edges
+	# 			if p[i][v] == v:
+	# 				break	
+	# 			v = p[i][v]
+	# 		if flag:
+	# 			break
+	g_1 = [r[:] for r in g]
+	for k in lst:
+		for i in lst:
+			for j in lst:
+				if g_1[i][k] + g_1[k][j] < g_1[i][j]:
+					g_1[i][j] = g_1[i][k] + g_1[k][j]
 					c[i][j] = True
-					break
+
+# def test():
+# 	import networkx as nx
+# 	import random
+# 	weights = range(-1000, 1001)
+# 	n, p = 150, 1
+# 	inf  = float('inf')
+# 	lst = xrange(n)
+# 	g = nx.fast_gnp_random_graph(n, p, directed=True)
+# 	g1 = [[inf if j != i else 0 for j in lst ] for i in lst]
+# 	p = [[None] * n for _ in lst]
+# 	c = [[False] * n for _ in lst]
+# 	# Add weights
+# 	for i, j in g.edges():
+# 		w = random.choice(weights)
+# 		g[i][j]['weight'] = g1[i][j] = w
+# 	# d1 = [r[:] for r in g1]
+# 	floyd_warshall(g1, p, c)
+# 	d1 = nx.floyd_warshall(g)
+
+# 	for i in lst:
+# 		for j in lst:
+# 			if d1[i][j] != g1[i][j]:
+# 				print (i, j), d1[i][j], g1[i][j]
+
+	
+
 
 def main():
 	first = True
-	max_w = 1e3 + 1
+	inf = float('inf')
 	while True:
 		get_input = lambda : map(int, raw_input().split())
 		n, m, q = get_input()
+		lst = xrange(n)
 		if n + m + q == 0:
 			break
 		if not first:
 			print
 		else:
 			first = False
-		g = [[max_w] * n for _ in xrange(n)]
-		t = [[False] * n for _ in xrange(n)]
-		c = [[False] * n for _ in xrange(n)]
+		g = [[inf if j != i else 0 for j in lst] for i in lst]
+		p = [[None] * n for _ in lst]
+		c = [[False] * n for _ in lst]
 		for _ in xrange(m):
 			u, v, w = get_input()
-			g[u][v] = w
-			t[u][v] = True
-		floyd_warshall(g, t, c)
+			g[u][v] = min(g[u][v], w) # Assuming Multigraphs
+			p[u][v] = u
+		floyd_warshall(g, p, c)
 		for _ in xrange(q):
 			u, v = get_input()
-			if t[u][v]:
-				print 0 if u == v else g[u][v] if not c[u][v] else '-Infinity'
+			if g[u][v] != inf:
+				if c[u][v]:
+					print '-Infinity'
+				else:
+					print 0 if u == v else g[u][v]
 			else:
 				print 'Impossible'
 			
